dc7cf21749695ab3f32dc1d7e74fd64491a8bb7d
chenjiaqi <chenjiaqi77@qq.com>
2018-08-21 09:08:53 +0800

    modify

--- main.c
+++ main.c
@@ -4,10 +4,17 @@
 #include "sim.h"
 int main(void) {
   printf("hello world\n");
+  sim_cfg_t cfg = {
+    .page_size = 2048,
+    .spare_size = 64,
+    .block_num = 1024,
+    .pages_per_block = 64,
+  };
   sim_t * sim = sim_allocate();
   if(!sim) {
     printf("sim allocate error");
   }
-  sim->ops->init(sim, NULL);
+  sim->ops->init(sim, &cfg);
+  sim->ops->erase(sim, 0);
   return 0;
 }
--- sim.c
+++ sim.c
@@ -16,25 +16,36 @@ static int init(sim_t *obj, const sim_cfg_t *cfg) {
     return -1;
   }
 
-  priv_t *priv = (priv_t *)malloc(sizeof(priv_t))
+  priv_t *priv = (priv_t *)malloc(sizeof(priv_t));
 
-  if(!obj->priv) {
-    printf("ERROR:memory\n");
+  if(!priv) {
+    printf("ERROR:priv memory \n");
     return -1;
   }
 
   memset(priv, 0, sizeof(priv_t));
 
   priv->cfg = *cfg;
-
+  printf("total size is %u\n", (cfg->page_size + cfg->spare_size) * cfg->pages_per_block * cfg->block_num);
   priv->data = (uint8_t *)malloc((cfg->page_size + cfg->spare_size) * cfg->pages_per_block * cfg->block_num);
-
+  memset(priv->data, 0xff, (cfg->page_size + cfg->spare_size) * cfg->pages_per_block * cfg->block_num);
   if(!priv->data) {
-    printf("ERROR:memory\n");
+    printf("ERROR:priv data memory\n");
   }
+
+  obj->priv = priv;
   return 0;
 }
 
+static int erase(sim_t *obj, uint32_t block) {
+  if(!obj || !obj->priv) {
+    printf("ERROR:params");
+    return -1;
+  }
+  priv_t *priv = (priv_t *)obj->priv;
+  printf("%d, %d, %d, %d\n", priv->cfg.page_size, priv->cfg.spare_size, priv->cfg.pages_per_block, priv->cfg.block_num);
+  return 0;
+}
 sim_t * sim_allocate(void) {
   sim_t * sim = (sim_t *) malloc(sizeof(sim_t));
   if(!sim) {
@@ -44,7 +55,7 @@ sim_t * sim_allocate(void) {
   memset(sim, 0, sizeof(*sim));
   static const sim_ops_t ops = {
     .init = init,
-    .erase = NULL,
+    .erase = erase,
     .read = NULL,
     .write = NULL,
   };
